109b927965a66b4342f19b882dfe70b4e8f73846
Ederson <ederson.mangueira@hotmail.com>
2019-06-08 16:51:14 -0300

    Validando token

--- src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
+++ src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
@@ -1,12 +1,29 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dto.ConfirmacaoLoginForm;
+import br.com.caelum.ingresso.helper.TokenHelper;
+import br.com.caelum.ingresso.mail.EmailNovoUsuario;
+import br.com.caelum.ingresso.model.Mailer;
+import br.com.caelum.ingresso.model.Token;
 
 @Controller
 public class UsuarioController {
+	
+	@Autowired
+	private Mailer mailer;
+	
+	@Autowired
+	private TokenHelper tokenHelper;
+	
 	
 	@GetMapping("/usuario/request")
 	public ModelAndView formSolicitacaoDeAcesso() {
@@ -17,7 +34,34 @@ public class UsuarioController {
 	public ModelAndView solicitacaoDeAcesso(String email) {
 
 		ModelAndView view = new ModelAndView("usuario/adicionado");
+		
+		Token token = tokenHelper.generateFrom(email);
+		
+		mailer.send(new EmailNovoUsuario(token));
 
+		return view;
+	}
+	
+	@GetMapping("/usuario/validate")
+	public ModelAndView validaLink(@RequestParam("uuid") String uuid) {
+		
+		Optional<Token> optionalToken = tokenHelper.getTokenFrom(uuid);
+		
+		if(!optionalToken.isPresent()) {
+			ModelAndView view = new ModelAndView("redirect:/login");
+			
+			view.addObject("msg", "O token do link utilizado não foi encontrado");
+			
+			return view;
+		}
+		Token token = optionalToken.get();
+		
+		ConfirmacaoLoginForm confirmacaoLoginForm = new ConfirmacaoLoginForm(token);
+		
+		ModelAndView view = new ModelAndView("usuario/confirmacao");
+		
+		view.addObject("confirmacaoLoginForm", confirmacaoLoginForm);
+		
 		return view;
 	}
 }
